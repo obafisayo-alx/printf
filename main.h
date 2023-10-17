1edae04059219d5d05e9f61667eadc05448cc37b
obafisayo-alx <sekaniisjoy@gmail.com>
2023-10-17 21:30:18 +0100

    debug and corrected betty checks

--- main.h
+++ main.h
@@ -28,8 +28,8 @@
  */
 struct fmt
 {
-    char fmt;
-    int (*fn)(va_list, char[], int, int, int, int);
+	char fmt;
+	int (*fn)(va_list, char[], int, int, int, int);
 };
 
 
@@ -43,7 +43,7 @@ typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
-			  va_list list, char buffer[], int flags, int width, int precision, int size);
+va_list list, char buffer[], int flags, int width, int precision, int size);
 
 
 
